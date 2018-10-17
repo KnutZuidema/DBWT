703730e2f8b965c02a49ed8dc329e9069419cc6b
Knut Zuidema <knut.zuidema@gmail.com>
2018-10-17 05:48:03 +0200

    worked on details page

--- Milestone 1/milestone_1.py
+++ Milestone 1/milestone_1.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,5 +13,10 @@ def products():
     return render_template('products.html')
 
 
+@app.route('/products/<name>')
+def details(name: str):
+    return render_template('details.html', name=name)
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=7890)
+    app.run(host='localhost', port=7890, debug=True)
