a857714ebe25630960beacde05d63678332efd13
Knut Zuidema <knut.zuidema@gmail.com>
2018-10-23 02:19:45 +0200

    add ability to register

--- Milestone_1/routes.py
+++ Milestone_1/routes.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, render_template, request, redirect, make_response
 from passlib.hash import argon2
 
-from database import SQLSession
+from database import SQLSession, User
 from login import init_login
 
 
@@ -60,6 +60,25 @@ def init_app():
         response = make_response(redirect(route))
         response.set_cookie('username', '', expires=0)
         response.set_cookie('hash', '', expires=0)
+        return response
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if request.method == 'GET':
+            return render_template('register.html')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        route = request.form.get('route')
+        sql_session = SQLSession(sql_config)
+        user = sql_session.get_user(username)
+        if user:
+            return 'User existiert bereits'
+        hash = argon2.hash(password)
+        sql_session.add(User(name=username, password=hash))
+        sql_session.commit()
+        response = make_response(redirect(route))
+        response.set_cookie('username', username)
+        response.set_cookie('hash', argon2.hash(hash))
         return response
 
     return app
