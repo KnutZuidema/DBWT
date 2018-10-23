3a46539e142a78a8b5b4563cdd039a54c9e68df4
Knut Zuidema <knut.zuidema@gmail.com>
2018-10-23 05:34:35 +0200

    refactor filter logic

--- Milestone_1/static/js/products.js
+++ Milestone_1/static/js/products.js
@@ -1,38 +1,47 @@
 $(function () {
     let checkboxes = $(':checkbox');
     let cards = $('.card');
-    checkboxes.click(function () {
+    checkboxes.each(function () {
         let checkbox = $(this);
         let selected = checkbox.attr('id');
-        if(checkbox.is(':checked')){
-            cards.each(function () {
-                let card = $(this);
-                if(!card.hasClass(selected)){
-                    card.hide();
-                }
-            })
+        if (checkbox.is(':checked')) {
+            hide_without_class(cards, selected);
+        }
+    });
+    checkboxes.click(function () {
+        cards.show();
+        let checkbox = $(this);
+        let selected = checkbox.attr('id');
+        if (checkbox.is(':checked')) {
+            hide_without_class(cards, selected);
         }else{
-            let checked = [];
-            checkboxes.each(function () {
-                let checkbox = $(this);
-                if(checkbox.is(':checked') && checkbox.attr('id') !== selected){
-                    checked.push(checkbox.attr('id'));
-                }
-            });
-            cards.show();
+            let checked = get_other_checked(checkboxes, selected);
             if(!checked.length){
                 return
             }
-            cards.each(function () {
-                let card = $(this);
-                let class_list = card.attr('class').split(/\s+/);
-                for(let check of checked){
-                    if(!class_list.includes(check)){
-                        card.hide();
-                        break
-                    }
-                }
+            checked.forEach(function (check) {
+                hide_without_class(cards, check)
             })
         }
     })
 });
+
+function hide_without_class(elements, clazz){
+    elements.each(function () {
+        let element = $(this);
+        if (!element.hasClass(clazz)) {
+            element.hide();
+        }
+    })
+}
+
+function get_other_checked(checkboxes, this_value){
+    let checked = [];
+    checkboxes.each(function () {
+        let checkbox = $(this);
+        if (checkbox.is(':checked') && checkbox.attr('id') !== this_value) {
+            checked.push(checkbox.attr('id'));
+        }
+    });
+    return checked
+}
